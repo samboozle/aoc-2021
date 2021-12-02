88c79c57bb88a9d03a1ef8ddaddb283ee13508b8
samboozle <samuel@samuelb01.local>
2021-12-02 13:01:24 -0800

    implement solutions to day two puzzles

--- src/advent_clj/core.clj
+++ src/advent_clj/core.clj
@@ -5,6 +5,7 @@
 ;; Dec 01 puzzles
 
 (def DAY-ONE-INPUT
+  ;; input formatted as a vector of integers 
   (-> "input-01.txt"
       io/resource
       slurp
@@ -16,16 +17,53 @@
           [acc line]
           [(inc acc) line]))
 
-(defn day-one-star-one []
+(defn find-day-one-star [day]
+  ())
+
+(def day-one-star-one
   (first (reduce count-increases-over-prior [0 nil] DAY-ONE-INPUT)))
 
-(defn day-one-star-two []
+(def day-one-star-two
   (first (reduce count-increases-over-prior
                  [0 nil]
                  (map (partial reduce +) (partition 3 1 DAY-ONE-INPUT)))))
 
 ;; Dec 02 puzzles
 
-(defn day-two-star-one [] "Unimplemented")
+(def DAY-TWO-INPUT
+  ;; input formatted as a vector of [keyword integer] tuples (vectors)
+  (-> "input-02.txt"
+      io/resource
+      slurp
+      (string/split #"\n")
+      ((partial map
+                (comp
+                 (partial apply #(vector (keyword %1) (read-string %2)))
+                 #(string/split % #"\s"))))))
 
-(defn day-two-star-two [] "Unimplemented")
+(defn change-point [[hrz dep] [dir amp]]
+  (case dir
+    :forward [(+ hrz amp) dep]
+    :down    [hrz (+ dep amp)]
+    :up      [hrz (max (- dep amp) 0)]))
+
+(defn change-point' [[hrz dep aim] [dir amp]]
+  (case dir
+    :forward [(+ hrz amp) (+ dep (* aim amp)) aim]
+    :down    [hrz dep (+ aim amp)]
+    :up      [hrz dep (max (- aim amp) 0)]))
+
+(def day-two-star-one
+  (reduce * (reduce change-point [0 0] DAY-TWO-INPUT)))
+
+(def day-two-star-two
+  (reduce * (take 2 (reduce change-point' [0 0 0] DAY-TWO-INPUT))))
+
+;; overall result
+
+(def SOLUTION-MAP
+  {:one {:one day-one-star-one :two day-one-star-two}
+   :two {:one day-two-star-one :two day-two-star-two}})
+
+(defn find-star [day star]
+  (get-in SOLUTION-MAP [day star]))
